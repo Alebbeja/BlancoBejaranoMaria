5700b9d3c5e38944225f8ef5a3a3df0a091694ea
Alebbeja <Maria.Blanco@fundacion-jala.org>
2023-07-19 08:20:48 -0400

    class 2

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -5,29 +5,42 @@ from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 
 # la clase Spaceship va a heredar la clase Sprite
 class Spaceship(Sprite):
-    SPACESHIP_WIDTH = 40
-    SPACESHIP_WEIGTH = 60
+    SPACESHIP_WIDTH = 100
+    SPACESHIP_WEIGHT = 60
     SPACESHIP_POS_X = SCREEN_WIDTH / 2
     SPACESHIP_POS_Y = 500
 
     def __init__(self):
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image, (self.SPACESHIP_WIDTH, self.SPACESHIP_WEIGTH))
+        self.image = pygame.transform.scale(self.image, (self.SPACESHIP_WIDTH, self.SPACESHIP_WEIGHT))
         self.rect = self.image.get_rect(midbottom = (self.SPACESHIP_POS_X, self.SPACESHIP_POS_Y))
 
     def update(self, user_input):
-        if user_input[pygame.K_LEFT] and self.rect.x > 0:
+        if user_input[pygame.K_LEFT]:
             self.rect.x -= 10
-            if self.rect.right < 0:
+            if self.rect.right <= 0:
                 self.rect.left = SCREEN_WIDTH
-        elif user_input[pygame.K_RIGHT] and self.rect.x < SCREEN_WIDTH:
+
+        elif user_input[pygame.K_RIGHT]:
             self.rect.x += 10
-            if self.rect.left> SCREEN_WIDTH:
-                self.rect.right = 0
+            if self.rect.left >= SCREEN_WIDTH:
+                self.rect.left = -self.SPACESHIP_WIDTH
+
         elif user_input[pygame.K_UP] and self.rect.top > 300:
             self.rect.y -= 10
+
         elif user_input[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
             self.rect.y += 10
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect (200, 500))
+        screen.blit(self.image, self.rect)
+
+    def move_right(self):
+        self.rect.x +=10
+        if self.rect.right >= SCREEN_WIDTH:
+            self.rect.left = -self.SPACESHIP_WIDTH
+
+    def move_left(self):
+        self.rect.x -=10
+        if self.rect.left <= 0:
+            self.rect.right =SCREEN_WIDTH - self.SPACESHIP_WIDTH
